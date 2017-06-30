3ca045761eb12d94988688b963025e2fe9538cd5
Matthew Vukomanovic <matthew.vukomanovic@gmail.com>
2017-06-30 21:05:59 +0800

    Include a large set of choco installs

---
+++ chocoinstall.bat
@@ -0,0 +1,131 @@
+choco install 7zip -y
+call RefreshEnv.cmd
+choco install autohotkey -y
+call RefreshEnv.cmd
+choco install openvpn -y
+call RefreshEnv.cmd
+choco install linqpad -y
+call RefreshEnv.cmd
+choco install ditto -y
+call RefreshEnv.cmd
+choco install gitextensions -y
+call RefreshEnv.cmd
+choco install urlrewrite -y
+call RefreshEnv.cmd
+choco install imdisk -y
+call RefreshEnv.cmd
+choco install dependencywalker  -y
+call RefreshEnv.cmd
+choco install stylecop -y
+call RefreshEnv.cmd
+choco install iis-application-initialization -y
+call RefreshEnv.cmd
+choco install iis-media-services -y
+call RefreshEnv.cmd
+choco install agentransack  -y
+call RefreshEnv.cmd
+choco install freesshd -y
+call RefreshEnv.cmd
+choco install gitkraken -y
+call RefreshEnv.cmd
+choco install universal-usb-installer -y
+call RefreshEnv.cmd
+choco install msbuild.communitytasks  -y
+call RefreshEnv.cmd
+choco install papercut -y
+call RefreshEnv.cmd
+choco install adexplorer -y
+call RefreshEnv.cmd
+choco install selenium-chrome-driver -y
+call RefreshEnv.cmd
+choco install mysql.workbench -y
+call RefreshEnv.cmd
+choco install mysql -y
+call RefreshEnv.cmd
+choco install mono -y
+call RefreshEnv.cmd
+choco install superantispyware -y
+call RefreshEnv.cmd
+choco install inkscape -y
+call RefreshEnv.cmd
+choco install notepadplusplus.install -y
+call RefreshEnv.cmd
+choco install skype -y
+call RefreshEnv.cmd
+choco install jdk8 -y
+call RefreshEnv.cmd
+choco install filezilla -y
+call RefreshEnv.cmd
+choco install paint.net -y
+call RefreshEnv.cmd
+choco install python2 -y
+call RefreshEnv.cmd
+choco install irfanview -y
+call RefreshEnv.cmd
+choco install visualstudiocode -y
+call RefreshEnv.cmd
+choco install fiddler4 -y
+call RefreshEnv.cmd
+choco install stexbar -y
+call RefreshEnv.cmd
+
+choco install greenshot -y
+call RefreshEnv.cmd
+choco install hg -y
+call RefreshEnv.cmd
+choco install winmerge -y
+call RefreshEnv.cmd
+choco install mingw -y
+call RefreshEnv.cmd
+choco install pidgin -y
+call RefreshEnv.cmd
+choco install slack -y
+call RefreshEnv.cmd
+choco install ilspy -y
+call RefreshEnv.cmd
+choco install clink -y
+call RefreshEnv.cmd
+choco install sudo -y
+call RefreshEnv.cmd
+choco install adblockplus-firefox -y
+call RefreshEnv.cmd
+choco install linkshellextension -y
+call RefreshEnv.cmd
+choco install rsat -y
+call RefreshEnv.cmd
+
+
+choco install gnuwin -y
+call RefreshEnv.cmd
+choco install sysinternals -y
+
+rem choco install smartgit
+rem https://chocolatey.org/packages/papercut
+rem 
+rem 
+rem NetBalancer 9.5.2
+rem Growl
+rem choco install chromedriver2 
+rem https://chocolatey.org/packages/ChromeDriver2
+rem 
+rem choco install soapui 
+rem choco install activeperl 
+rem choco install adexplorer 
+rem 
+rem 
+rem choco install adfender 
+rem 
+rem choco install android-sdk 
+rem choco install androidstudio
+rem choco install avastfreeantivirus 
+rem choco install bginfo 
+rem https://chocolatey.org/packages?sortOrder=package-title&page=16&prerelease=False&moderatorQueue=False
+rem https://chocolatey.org/packages?sortOrder=package-title&page=20&prerelease=False&moderatorQueue=False
+rem choco install -y vcbuildtools
+rem 
+rem 
+rem 
+rem start /wait vs_professional.exe /productkey KB7P7-JN8MX-WCGPW-9RCBG-683CC /promptrestart /passive
+rem 
+rem 
+rem https://az764295.vo.msecnd.net/stable/19222cdc84ce72202478ba1cec5cb557b71163de/VSCodeSetup-1.12.2.exe
