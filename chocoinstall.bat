d3d2f1517e91ec5a7d56e6ed3d02afee755e1694
Matthew Vukomanovic <matthew.vukomanovic@gmail.com>
2017-07-14 01:28:36 +0800

    Update the choco installs

--- chocoinstall.bat
+++ chocoinstall.bat
@@ -1,80 +1,54 @@
+choco install git -y
+call RefreshEnv.cmd
 choco install vim -y
 call RefreshEnv.cmd
 choco install everything -y
-call RefreshEnv.cmd
-choco install conemu -y
-call RefreshEnv.cmd
-choco install nugetpackageexplorer -y
 call RefreshEnv.cmd
 choco install 7zip -y
 call RefreshEnv.cmd
 choco install autohotkey -y
 call RefreshEnv.cmd
+choco install conemu -y
+call RefreshEnv.cmd
 choco install openvpn -y
 call RefreshEnv.cmd
 choco install linqpad -y
 call RefreshEnv.cmd
+choco install stexbar -y
+call RefreshEnv.cmd
 choco install ditto -y
-call RefreshEnv.cmd
-choco install gitextensions -y
-call RefreshEnv.cmd
-choco install urlrewrite -y
 call RefreshEnv.cmd
 choco install imdisk -y
 call RefreshEnv.cmd
-choco install dependencywalker  -y
-call RefreshEnv.cmd
-rem choco install stylecop -y
-call RefreshEnv.cmd
-choco install iis-application-initialization -y
-call RefreshEnv.cmd
-choco install iis-media-services -y
-call RefreshEnv.cmd
 choco install agentransack  -y
 call RefreshEnv.cmd
-choco install freesshd -y
+choco install sysinternals -y
 call RefreshEnv.cmd
-choco install gitkraken -y
+choco install fiddler4 -y
+call RefreshEnv.cmd
+choco install ilspy -y
+call RefreshEnv.cmd
+choco install clink -y
+call RefreshEnv.cmd
+choco install sudo -y
+call RefreshEnv.cmd
+choco install rsat -y
+call RefreshEnv.cmd
+
+
+choco install freesshd -y
 call RefreshEnv.cmd
 choco install universal-usb-installer -y
 call RefreshEnv.cmd
 choco install msbuild.communitytasks  -y
 call RefreshEnv.cmd
-choco install papercut -y
+
+choco install firefox -y
 call RefreshEnv.cmd
-choco install adexplorer -y
+choco install adblockplus-firefox -y
 call RefreshEnv.cmd
-choco install selenium-chrome-driver -y
-call RefreshEnv.cmd
-choco install mysql.workbench -y
-call RefreshEnv.cmd
-choco install mysql -y
-call RefreshEnv.cmd
-choco install mono -y
-call RefreshEnv.cmd
-choco install superantispyware -y
-call RefreshEnv.cmd
-choco install inkscape -y
-call RefreshEnv.cmd
+
 choco install notepadplusplus.install -y
-call RefreshEnv.cmd
-choco install skype -y
-call RefreshEnv.cmd
-choco install jdk8 -y
-call RefreshEnv.cmd
-choco install filezilla -y
-call RefreshEnv.cmd
-choco install paint.net -y
-call RefreshEnv.cmd
-choco install python2 -y
-call RefreshEnv.cmd
-choco install irfanview -y
-call RefreshEnv.cmd
-choco install visualstudiocode -y
-call RefreshEnv.cmd
-choco install fiddler4 -y
-call RefreshEnv.cmd
-choco install stexbar -y
 call RefreshEnv.cmd
 
 choco install greenshot -y
@@ -85,28 +59,77 @@ choco install winmerge -y
 call RefreshEnv.cmd
 choco install mingw -y
 call RefreshEnv.cmd
+
+choco install vcredist2005 -y
+call RefreshEnv.cmd
+
+choco install dependencywalker  -y
+call RefreshEnv.cmd
+
+rem choco install stylecop -y
+rem call RefreshEnv.cmd
+
+choco install papercut -y
+call RefreshEnv.cmd
+choco install adexplorer -y
+call RefreshEnv.cmd
+choco install selenium-chrome-driver -y
+call RefreshEnv.cmd
+choco install inkscape -y
+call RefreshEnv.cmd
+choco install filezilla -y
+call RefreshEnv.cmd
+choco install visualstudiocode -y
+call RefreshEnv.cmd
+
+choco install superantispyware -y --ignore-checksums
+call RefreshEnv.cmd
+
+choco install irfanview -y
+call RefreshEnv.cmd
+choco install mysql.workbench -y
+call RefreshEnv.cmd
+choco install mysql -y
+call RefreshEnv.cmd
+choco install skype -y
+call RefreshEnv.cmd
+choco install jdk8 -y
+call RefreshEnv.cmd
+
 choco install pidgin -y
 call RefreshEnv.cmd
 choco install slack -y
 call RefreshEnv.cmd
-choco install ilspy -y
-call RefreshEnv.cmd
-choco install clink -y
-call RefreshEnv.cmd
-choco install sudo -y
-call RefreshEnv.cmd
-choco install adblockplus-firefox -y
-call RefreshEnv.cmd
-choco install linkshellextension -y
-call RefreshEnv.cmd
-choco install rsat -y
+
+choco install paint.net -y
 call RefreshEnv.cmd
 
+choco install nugetpackageexplorer -y
+call RefreshEnv.cmd
+
+choco install gitextensions -y
+call RefreshEnv.cmd
+
+choco install python2 -y
+call RefreshEnv.cmd
 
 choco install gnuwin -y
 call RefreshEnv.cmd
-choco install sysinternals -y
+
+choco install mono -y
 call RefreshEnv.cmd
+
+choco install iis-application-initialization -y
+call RefreshEnv.cmd
+choco install iis-media-services -y
+call RefreshEnv.cmd
+
+choco install urlrewrite -y
+call RefreshEnv.cmd
+
+rem choco install linkshellextension -y
+rem call RefreshEnv.cmd
+
 
 rem choco install smartgit
 rem https://chocolatey.org/packages/papercut
@@ -138,3 +161,8 @@ rem start /wait vs_professional.exe /productkey KB7P7-JN8MX-WCGPW-9RCBG-683CC /p
 rem 
 rem 
 rem https://az764295.vo.msecnd.net/stable/19222cdc84ce72202478ba1cec5cb557b71163de/VSCodeSetup-1.12.2.exe
+
+
+
+choco install gitkraken -y
+call RefreshEnv.cmd
